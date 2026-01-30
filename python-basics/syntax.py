e725dda4986a463dfb8d1ed712819ea3ed76535d
Akniyet <aknietnroe@gmail.com>
2026-01-30 23:48:10 +0500

    Create syntax.py

--- python-basics/syntax.py
+++ python-basics/syntax.py
@@ -1,10 +1,40 @@
 # Example 1
-print("Hello, World!")
+if 5 > 2:
+  print("Five is greater than two!")
 
 # Example 2
-# This is a comment
-print("Hello, World!")
+if 5 > 2:
+print("Five is greater than two!")
 
 # Example 3
-"""This is a multiline docstring."""
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+# Example 4
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+# Example 5
+x = 5
+y = "Hello, World!"
+
+# Example 6
+#This is a comment.
 print("Hello, World!")
+
+# Example 7
+print("Python is fun!")
+
+# Example 8
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+# Example 9
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+# Example 10
+print("Python is fun!") print("Really!")
