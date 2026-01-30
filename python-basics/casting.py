aba2f8d16d0e10e8e1a131c248a25aa3cfaa0368
Akniyet <aknietnroe@gmail.com>
2026-01-30 23:56:23 +0500

    Create casting.py

--- python-basics/casting.py
+++ python-basics/casting.py
@@ -2,24 +2,14 @@
 x = int(1)
 y = int(2.8)
 z = int("3")
-print(x)
-print(y)
-print(z)
 
 # Example 2
-x = float(1)
-y = float(2.8)
-z = float("3")
-w = float("4.2")
-print(x)
-print(y)
-print(z)
-print(w)
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
 
 # Example 3
-x = str("s1")
-y = str(2)
-z = str(3.0)
-print(x)
-print(y)
-print(z)
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
