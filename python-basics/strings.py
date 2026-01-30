ff82378f2cea8874c11c61110a3f95a4142a4683
Akniyet <aknietnroe@gmail.com>
2026-01-30 23:31:14 +0500

    Create strings.py

---
+++ python-basics/strings.py
@@ -0,0 +1,32 @@
+# Example 1
+a = "Hello, World!"
+print(a[1])
+
+# Example 2
+b = "Hello, World!"
+print(b[2:5])
+
+# Example 3
+a = " Hello, World! "
+print(a.strip())
+
+# Example 4
+a = "Hello, World!"
+print(len(a))
+
+# Example 5
+a = "Hello, World!"
+print(a.lower())
+
+# Example 6
+a = "Hello, World!"
+print(a.upper())
+
+# Example 7
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# Example 8
+a = "Hello, World!"
+b = a.split(",")
+print(b)
