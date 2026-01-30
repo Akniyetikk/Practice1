f5c37a1be83c8ae507ebcb6cc2edb630837ed014
Akniyet <aknietnroe@gmail.com>
2026-01-30 23:53:38 +0500

    Create datatypes.py

---
+++ python-basics/datatypes.py
@@ -0,0 +1,15 @@
+# Example 1
+x = 5
+print(type(x))
+
+# Example 2
+x = "Hello World"
+
+# Example 3
+x = ["apple", "banana", "cherry"]
+
+# Example 4
+x = {"name" : "John", "age" : 36}
+
+# Example 5
+x = range(6)
