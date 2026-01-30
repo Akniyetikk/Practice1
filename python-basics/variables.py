da7af56b1a758a2542b52ce10113eb2e014eb9c4
Akniyet <aknietnroe@gmail.com>
2026-01-30 23:30:09 +0500

    Create variables.py

---
+++ python-basics/variables.py
@@ -0,0 +1,17 @@
+# Example 1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Example 2
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+# Example 3
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
